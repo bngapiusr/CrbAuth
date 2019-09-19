09bbd1a4acd72f3e22b145866d69b89e945dcd5d
Tony Ochiche <aochiche@msn.com>
2019-09-19 12:37:23 -0500

    Add Generic Interface...

---
+++ CrbAuth.Data/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CrbAuth.Data.Interfaces
+{
+    public interface IRepository <TEntity, TKey> where TEntity : class
+    {
+        IEnumerable<TEntity> All();
+        TEntity Find(TKey key);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity key);
+    }
+}
