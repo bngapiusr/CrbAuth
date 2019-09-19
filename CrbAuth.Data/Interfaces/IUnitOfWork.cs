ccda4c6305fdcc0232be3eb83fe21cd2419c2743
Tony Ochiche <aochiche@msn.com>
2019-09-19 14:44:52 -0500

    Add Unit Of Work  Interface...

---
+++ CrbAuth.Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CrbAuth.Data.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRoleRepository RoleRepository { get; }
+        IUserRepository UserRepository { get; }
+        //IRepository<UserToken,UserTokenKey> UserTokenRepository { get; }
+        IUserRepository UrserRoleRepository { get; }
+
+        void Commit();
+
+    }
+}
