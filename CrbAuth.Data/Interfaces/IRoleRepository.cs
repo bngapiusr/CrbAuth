148443c192eb7e7ed2a4c0ce0e214be188ba2a1d
Tony Ochiche <aochiche@msn.com>
2019-09-19 12:44:02 -0500

    Add Role Interface...

---
+++ CrbAuth.Data/Interfaces/IRoleRepository.cs
@@ -0,0 +1,10 @@
+﻿using CrbAuth.Entities;
+
+namespace CrbAuth.Data.Interfaces
+{
+    public interface IRoleRepository : IRepository<Role, string>
+    {
+        Role FindBName(string roleName);
+
+    }
+}
