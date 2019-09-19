9c10e448bdafb72c1139ac5a93e5c41aaca59d9b
Tony Ochiche <aochiche@msn.com>
2019-09-19 14:18:44 -0500

    Add UserRole Interface...

---
+++ CrbAuth.Data/Interfaces/IUserRoleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using CrbAuth.Entities;
+
+namespace CrbAuth.Data.Interfaces
+{
+    public interface IUserRoleRepository
+    {
+        void Add(string userId, string roleName);
+        void Remove(string userId, string roleName);
+        IEnumerable<string> GetRoleNamesByUserId(string userId);
+        IEnumerable<User> GetUsersByRoleName(string roleName);
+    }
+}
