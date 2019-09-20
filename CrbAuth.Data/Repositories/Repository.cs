a1cd59777c941802c2c654552f64807949aff056
Tony Ochiche <aochiche@msn.com>
2019-09-20 11:08:45 -0500

    Add Empty Repository class

---
+++ CrbAuth.Data/Repositories/Repository.cs
@@ -0,0 +1,10 @@
+﻿using CrbAuth.Data.Interfaces;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace CrbAuth.Data.Repositories
+{
+    //public class Repository : IRepository<T, Key> where T : class
+    //{
+        
+    //}
+}
