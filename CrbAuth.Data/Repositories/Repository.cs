65d9d98c249a420cb97054a602fab84fbca954b9
Tony <geekdev01@gmail.com>
2019-09-21 05:50:14 -0500

    Delete commented code from repository class...

--- CrbAuth.Data/Repositories/Repository.cs
+++ CrbAuth.Data/Repositories/Repository.cs
@@ -3,8 +3,5 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace CrbAuth.Data.Repositories
 {
-    //public class Repository : IRepository<T, Key> where T : class
-    //{
-        
-    //}
+
 }
