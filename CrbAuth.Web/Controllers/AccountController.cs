b83a8d03b76920432c1261f810281bb07e77f617
Tony <geekdev01@gmail.com>
2019-09-21 11:31:25 -0500

    Add Controllers folder and AccountController class...

---
+++ CrbAuth.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CrbAuth.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace CrbAuth.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+        public AccountController(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+
+    }
+}
