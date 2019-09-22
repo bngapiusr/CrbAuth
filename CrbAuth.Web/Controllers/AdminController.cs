4ceeb9ca2f61147a20f8f0b0b3fbd583e78e4881
Tony Ochiche <aochiche@msn.com>
2019-09-22 12:59:22 -0500

    Add AdminController, AdminViewModel and Admin\Index view...

---
+++ CrbAuth.Web/Controllers/AdminController.cs
@@ -0,0 +1,33 @@
+﻿using CrbAuth.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrbAuth.Web.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<Role> _roleManager;
+
+        public AdminController(UserManager<User> userManager, RoleManager<Role> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult UserManagement()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ CrbAuth.Web/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrbAuth.Web.ViewModels
+{
+    public class AddUserViewModel
+    {
+        public int UserId { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string   UserName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string FirstName { get; set; }
+        public string MiddleInitial { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string LastName { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string ConfirmedEmail { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8,ErrorMessage ="Password can not be less then 8 characters.")]
+        public string Password { get; set; }
+
+    }
+}
