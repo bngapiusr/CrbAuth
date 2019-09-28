53a4337e1c086bbd20dbd2182d45c0c1b5fe5b79
Tony Ochiche <aochiche@msn.com>
2019-09-28 17:10:21 -0500

    Add Toastr project with 3 classes

---
+++ CrbAuth.Toastr/OptionEnums/Position.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CrbAuth.Toastr.OptionEnums
+{
+    public enum Position
+    {
+        [Description("toast-top-right")]
+        TopRight,
+
+        [Description("toast-bottom-righ")]
+        BottomRight,
+
+        [Description("toast-bottom-left")]
+        BottomLeft,
+
+        [Description("toast-top-left")]
+        TopLeft,
+
+        [Description("toast-top-full-width")]
+        TopFullWidth,
+
+        [Description("toast-bottom-full-width")]
+        BottomFullWidth,
+
+        [Description("toast-top-center")]
+        TopCenter,
+
+        [Description("toast-bottom-center")]
+        BottomCenter
+    }
+}
---
+++ CrbAuth.Toastr/OptionEnums/ToastType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace CrbAuth.Toastr.OptionEnums
+{
+    public enum ToastType
+    {
+        [Description("success")]
+        Success,
+
+        [Description("info")]
+        Info,
+
+        [Description("warning")]
+        Warning,
+
+        [Description("error")]
+        Error,
+    }
+}
